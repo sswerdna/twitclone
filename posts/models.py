2e354a4c5e07de2e8e47c3573336be1cd848fe70
U-Stephen-PC\Stephen <Stephen@Stephen-PC.(none)>
2013-07-19 07:12:07 -0500

    Forgot to do nightly commit

--- posts/models.py
+++ posts/models.py
@@ -4,16 +4,17 @@ import datetime
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, related_name='profile')
     description = models.TextField()
-    following = models.ManyToManyField("self")
+    following = models.ManyToManyField("self",symmetrical=False,
+            related_name='followers')
     createdDate = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "{}'s Profile".format(self.user)
 
 class Post(models.Model):
-    user = models.ForeignKey(UserProfile)
+    user = models.ForeignKey(UserProfile, related_name='posts')
     content = models.CharField(max_length=140)
     createdDate = models.DateTimeField(auto_now_add = True)
 
--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,26 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from posts.models import UserProfile, Post
 
 def index(request):
     return render(request,"posts/frame.html.dj")
 
 def profile(request, username):
-    return render(request, "posts/frame.html.dj",{'user':username})
+    try: 
+        user = User.objects.get(username=username)
+        followers = user.profile.followers.all()
+        posts = user.profile.posts.all()
+        description = user.profile.description
+        return render(request, "posts/profile.html.dj",
+                {'username':username,'user':user,'followers':followers,
+                    'posts':posts, 'description':description})
+
+
+    except User.DoesNotExist:
+        return HttpResponse("Requested User {} does not exist".format(username))
+
 
 def activity(request, username=None):
-    return render(request, "posts/frame.html.dj", {'user':username})
-
+    return render(request, "posts/frame.html.dj", {'username':username})
