fd35b5753fe9c60432fd57eee9fd2357186be698
Stephen Andrews <sswerdna@gmail.com>
2013-07-22 15:04:44 -0500

    Post function works, made some minor templating and backend changes

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.core.context_processors import csrf
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
 from posts.models import UserProfile, Post
 
 def index(request):
@@ -10,38 +10,44 @@ def index(request):
 
 def profile(request, username):
     try: 
-        user = User.objects.get(username=username)
+        user = UserProfile.objects.get(user__username=username)
         current_user = request.session.get('username', None)
-        if current_user == user.username:
+        if current_user == user.usgr.username:
             return HttpResponseRedirect(reverse('posts:me'))
         else:
-            followers = user.profile.followers.all()
-            posts = user.profile.posts.order_by('createdDate')[:7]
-            description = user.profile.description
+            followers = user.followers.all()
+            posts = user.posts.order_by('createdDate')[:7]
             return render(request, "posts/profile.html.dj",
                     {'current_user':current_user,'user':user,
-                        'followers':followers,'posts':posts,
-                        'description':description})
+                        'followers':followers,'posts':posts.reverse(),
+                        })
 
 
-    except User.DoesNotExist:
+    except UserProfile.DoesNotExist:
         return HttpResponse("Requested User {} does not exist".format(username))
 
 
-def activity(request, username=None):
-    return render(request, "posts/frame.html.dj", {'username':username})
+def activity(request):
+    current_user = request.session.get(username, 'False')
+    if current_user:
+        user = UserProfile.objects.get(user__username=current_username)
 
 def me(request):
     current_username = request.session.get('username', False)
     if current_username:
+        user = UserProfile.objects.get(user__username=current_username)
         if request.method == 'POST':
-            pass
+            c = {}
+            c.update(csrf(request))
+            post_text = request.POST['post_text']
+            post = Post.objects.create(user=user, content=post_text)
+            post.save()
+            return HttpResponseRedirect(reverse('posts:me'), c)
         else:
-            user = User.objects.get(username=current_username)
-            posts = user.profile.posts.order_by('createdDate')[:7]
-            following = user.profile.following.all()
+            posts = user.posts.order_by('createdDate')[:7]
+            following = user.following.all()
             return render(request, "posts/me.html.dj",{'user':user,
-                'posts':posts, 'following':following, 
-                'current_user':user.username})
+                'posts':posts.reverse(), 'following':following, 
+                'current_user':current_username})
     else:
         return HttpResponseRedirect(request, 'login')
